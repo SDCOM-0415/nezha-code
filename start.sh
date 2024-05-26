8920c6200a2821e335d19de0a6bd447c553fa42f
SDCOM-0415 <127208666+SDCOM-0415@users.noreply.github.com>
2024-05-27 01:58:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,22 @@
-nohup ./nezha.sh &
+#!/bin/bash
+
+echo "请选择启动方式："
+echo "1. nezha.sh "
+echo "2. nezha1.sh "
+
+read -p "请输入数字 [1-2]: " num
+
+case $num in
+  1)
+    echo "正在以方式1启动..."
+    nohup bash ./nezha.sh &
+    ;;
+  2)
+    echo "正在以方式2启动..."
+    nohup bash ./nezha1.sh &
+    ;;
+  *)
+    echo "输入错误，请输入正确的数字 [1-2]"
+    ;;
+esac
+
